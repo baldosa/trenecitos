a7f63e7ebc1023f7599d5c3e3e9d23778f049022
Ema Guillén <ema@guillen.com.ar>
2024-06-23 12:32:10 -0300

    logo y footer

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,7 +3,30 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <header>
+    <figure>
+      <img src="/logo.png" />
+    </figure>
+  </header>
   <main class="container">
     <RouterView />
   </main>
+  <footer>
+    <nav>
+      <ul>
+        <li class="float-right">
+          <a href="https://github.com/baldosa/trenecitos" target="_blank">Github</a>
+        </li>
+      </ul>
+    </nav>
+  </footer>
 </template>
+
+<style>
+body {
+  padding-top: 0;
+}
+figure {
+  margin: 0;
+}
+</style>
